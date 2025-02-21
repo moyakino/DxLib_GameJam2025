89fcc0b23d7cf080968a239fe8f88b0b75dce290
shiona0820 <138412503+shiona0820@users.noreply.github.com>
2025-02-21 09:59:55 +0900

    0221
    画像の差し替え
    playerちょっと追加した

--- GameJam2025/Object/Player.cpp
+++ GameJam2025/Object/Player.cpp
@@ -1,7 +1,12 @@
 ﻿#include "Player.h"
 
-Player::Player()
+#include "DxLib.h"
+
+Player::Player():
+	player_state(ePlayerState::IDLE),
+	player_image(NULL)
 {
+
 }
 
 Player::~Player()
@@ -10,6 +15,24 @@ Player::~Player()
 
 void Player::Initialize(int pnum, float x)
 {
+	//画像の読み込み
+	switch (pnum)
+	{
+	case 0:
+		player_image = LoadGraph("Resource/images/player.bmp");
+		break;
+	case 1:
+		player_image = LoadGraph("Resource/images/player.bmp");
+		break;
+	default:
+		break;
+	}
+
+	//エラーチェック
+	if (player_image == -1)
+	{
+		throw("Resource / images / player.pngがありません\n");
+	}
 }
 
 void Player::Update()
@@ -18,6 +41,7 @@ void Player::Update()
 
 void Player::Draw() const
 {
+	DrawRotaGraphF(location.x, location.y, 1.0, angle, player_image, TRUE);
 }
 
 void Player::Finalize()
--- GameJam2025/Object/Player.h
+++ GameJam2025/Object/Player.h
@@ -1,10 +1,21 @@
 ﻿#pragma once
 #include"../Utility/Vector2D.h"
 
+//playerの状態
+enum ePlayerState
+{
+	IDLE,   //待機
+	WALK,   //歩く
+	SHOOT,   //撃つ
+};
+
 class Player
 {
 private:
+	int  player_image;   //playerの画像
 
+	Vector2D location;   //位置座標
+	float angle;         //角度
 
 
 
@@ -20,5 +31,6 @@ public:
 	void Draw()const;							//描画処理
 	void Finalize();							//終了時処理
 
+	ePlayerState player_state;   //playerの状態
 };
 
